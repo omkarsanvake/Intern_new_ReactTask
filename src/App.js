c811e7f9e1ddb153e0cadab651743c26f3ef14c8
omkarsanvake <omkarsanvake555@gmail.com>
2023-09-19 01:09:34 +0530

    my first react task

--- src/App.js
+++ src/App.js
@@ -1,25 +1,62 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import { CountdownCircleTimer } from "react-countdown-circle-timer";
+
+
+const renderTime = ({ remainingTime }) => {
+  if (remainingTime === 0) {
+    return <div className="timer">Too late...</div>;
+  }
+
+  return (
+    <div className="timer">
+      <div className="text">Remaining</div>
+      <div className="value">{remainingTime}</div>
+      <div className="text">seconds</div>
+    </div>
+  );
+};
 
 function App() {
+  const [isPaused, setIsPaused] = useState(false);
+  const [resetKey, setResetKey] = useState(0);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+      <h1>
+        CountdownCircleTimer
+        <br />
+        React Component
+      </h1>
+      <div className="timer-wrapper">
+        <CountdownCircleTimer
+          isPlaying={!isPaused}
+          key={resetKey}
+          duration={10}
+          colors={["#004777", "#F7B801", "#A30000", "#A30000"]}
+          colorsTime={[10, 6, 3, 0]}
+          onComplete={() => ({ shouldRepeat: true, delay: 1 })}
         >
-          Learn React
-        </a>
-      </header>
+          {renderTime}
+        </CountdownCircleTimer>
+      </div>
+      <div className="btn">
+        <button onClick={() => setIsPaused(prev => !prev)}>
+          {isPaused ? "Resume" : "Pause"}
+        </button>
+      </div>  
+      <div className="btn2">  
+      <button onClick={() => setResetKey(prev => prev + 1)}>
+        Reset
+      </button>
+      </div>
+
+      <p className="info">
+        Change component properties in the code filed on the right to try
+        different functionalities
+      </p>
     </div>
   );
 }
 
 export default App;
+
